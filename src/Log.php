c0c4ebce25620d66264dcab7d232a9b96b3856f3
dutchie027 <dutchie027@users.noreply.github.com>
2022-03-30 20:34:37 +0000

    Apply PHP-CS-Fixer changes

--- src/Log.php
+++ src/Log.php
@@ -7,79 +7,85 @@ namespace dutchie027\EasyMySQLBackup;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
 
-final class Log {
+final class Log
+{
+    protected static $instance;
 
-	protected static $instance;
+    /**
+     * Method to return the Monolog instance
+     *
+     * @return \Monolog\Logger
+     */
+    public static function getLogger()
+    {
+        if (!self::$instance) {
+            self::configureInstance();
+        }
 
-	/**
-	 * Method to return the Monolog instance
-	 *
-	 * @return \Monolog\Logger
-	 */
-	static public function getLogger()
-	{
-		if (! self::$instance) {
-			self::configureInstance();
-		}
+        return self::$instance;
+    }
 
-		return self::$instance;
-	}
+    /**
+     * Configure Monolog to use rotating files
+     */
+    protected static function configureInstance(): void
+    {
+        $dir = LOG_DIR . DIRECTORY_SEPARATOR . 'log';
 
-	/**
-	 * Configure Monolog to use rotating files
-	 *
-	 */
-	protected static function configureInstance() : void
-	{
-		$dir = LOG_DIR . DIRECTORY_SEPARATOR . 'log';
+        if (!file_exists($dir)) {
+            mkdir($dir, 0700, true);
+        }
 
-		if (!file_exists($dir)){
-			mkdir($dir, 0700, true);
-		}
+        $logger = new Logger('easyMySQLBackup');
+        $logger->pushHandler(new RotatingFileHandler($dir . DIRECTORY_SEPARATOR . 'easyMySQLBackup.log', Logger::DEBUG));
+        self::$instance = $logger;
+    }
 
-		$logger = new Logger('easyMySQLBackup');
-		$logger->pushHandler(new RotatingFileHandler($dir . DIRECTORY_SEPARATOR . 'easyMySQLBackup.log', Logger::DEBUG));
-		self::$instance = $logger;
-	}
+    // Log::info("something really interesting happened");
+    public static function debug($message, array $context = []): void
+    {
+        self::getLogger()->debug($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function debug($message, array $context = []) : void {
-		self::getLogger()->debug($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function info($message, array $context = []): void
+    {
+        self::getLogger()->info($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function info($message, array $context = []): void {
-		self::getLogger()->info($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function notice($message, array $context = []): void
+    {
+        self::getLogger()->notice($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function notice($message, array $context = []): void {
-		self::getLogger()->notice($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function warning($message, array $context = []): void
+    {
+        self::getLogger()->warning($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function warning($message, array $context = []): void {
-		self::getLogger()->warning($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function error($message, array $context = []): void
+    {
+        self::getLogger()->error($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function error($message, array $context = []): void {
-		self::getLogger()->error($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function critical($message, array $context = []): void
+    {
+        self::getLogger()->critical($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function critical($message, array $context = []): void {
-		self::getLogger()->critical($message, $context);
-	}
+    // Log::info("something really interesting happened");
+    public static function alert($message, array $context = []): void
+    {
+        self::getLogger()->alert($message, $context);
+    }
 
-    # Log::info("something really interesting happened");
-	public static function alert($message, array $context = []): void {
-		self::getLogger()->alert($message, $context);
-	}
-
-    # Log::info("something really interesting happened");
-    public static function emergency($message, array $context = []): void {
-		self::getLogger()->addEmergency($message, $context);
-	}
-
+    // Log::info("something really interesting happened");
+    public static function emergency($message, array $context = []): void
+    {
+        self::getLogger()->addEmergency($message, $context);
+    }
 }
--- src/S3.php
+++ src/S3.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace dutchie027\EasyMySQLBackup;
-use Aws\S3\S3Client;
+
 use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
 
 class S3
 {
-    /** @var object */
+    /**
+     * @var object
+     */
     private $s3;
 
-    /** @var array */
+    /**
+     * @var array
+     */
     private $bucketArray;
 
     public function __construct(array $settings)
@@ -21,55 +26,56 @@ class S3
             'credentials' => [
                 'key' => $settings['access_key'],
                 'secret' => $settings['secret_key'],
-            ]
+            ],
         ]);
-        
+
         $this->loadS3Buckets();
     }
 
     public function uploadFile($file, $bucket, $name = null)
-{
+    {
+        $key = (strlen($name) < 1) ? basename($file) : $name;
 
-    $key = (strlen($name) < 1 ) ? basename($file) : $name;
+        if (!in_array($bucket, $this->bucketArray, true)) {
+            $this->createS3Bucket($bucket);
+            $this->s3->waitUntil('BucketExists', ['Bucket' => $bucket]);
+            Log::info($bucket . " didn't exist. I'm creating it");
+        }
 
-    if (!in_array($bucket, $this->bucketArray)) {
-        $this->createS3Bucket($bucket);
-        $this->s3->waitUntil('BucketExists', ['Bucket' => $bucket]);
-        Log::info($bucket . " didn't exist. I'm creating it");
+        try {
+            $result = $this->s3->putObject([
+                'Bucket' => $bucket,
+                'Key' => $key,
+                'SourceFile' => $file,
+                'ACL' => 'private',
+            ]);
+        } catch (S3Exception $e) {
+            Log::error($e->getMessage() . ' ' . __FILE__ . ' ' . __LINE__);
+        }
     }
 
-    try {
-        $result = $this->s3->putObject([
-            'Bucket' => $bucket,
-            'Key' => $key,
-            'SourceFile' => $file,
-            'ACL' => 'private'
-        ]);
-    } catch (S3Exception $e) {
-        Log::error($e->getMessage() . " " . __FILE__ . " " . __LINE__);
-    }
-}
+    private function createS3Bucket($bucketName)
+    {
+        Log::info("Gonna make bucket $bucketName");
 
-private function createS3Bucket($bucketName)
-{
-    Log::info("Gonna make bucket $bucketName");
-    try {
-        $result = $this->s3->createBucket([
-            'Bucket' => $bucketName,
-        ]);
-    } catch (S3Exception $e) {
-        Log::error($e->getMessage() . " " . __FILE__ . " " . __LINE__);
+        try {
+            $result = $this->s3->createBucket([
+                'Bucket' => $bucketName,
+            ]);
+        } catch (S3Exception $e) {
+            Log::error($e->getMessage() . ' ' . __FILE__ . ' ' . __LINE__);
+        }
     }
-}
 
-private function loadS3Buckets()
-{
-    $this->bucketArray = [];
-    Log::debug("Loading Bucket Names");
-    $buckets = $this->s3->listBuckets();
-    foreach ($buckets['Buckets'] as $bucket) {
-        Log::debug("Adding bucket " . $bucket['Name'] . " to the array");
-        $this->bucketArray[] = $bucket['Name'];
+    private function loadS3Buckets()
+    {
+        $this->bucketArray = [];
+        Log::debug('Loading Bucket Names');
+        $buckets = $this->s3->listBuckets();
+
+        foreach ($buckets['Buckets'] as $bucket) {
+            Log::debug('Adding bucket ' . $bucket['Name'] . ' to the array');
+            $this->bucketArray[] = $bucket['Name'];
+        }
     }
-}
 }
