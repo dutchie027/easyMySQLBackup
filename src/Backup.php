7139f5f1c32892f3f32207992cf80074b912f78e
dutchie027 <dutchie027@users.noreply.github.com>
2022-03-30 23:16:47 +0000

    Apply PHP-CS-Fixer changes

--- src/Backup.php
+++ src/Backup.php
@@ -46,8 +46,6 @@ class Backup
      * @param bool   $compress
      *
      * @throws \Exception
-     *
-     * @return string
      */
     public function createLocalBackup($database, $compress = true): string
     {
