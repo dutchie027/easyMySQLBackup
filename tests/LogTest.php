286de095c6b01b3aa9e41b13fa01eceda69bfc02
dutchie027 <dutchie027@users.noreply.github.com>
2022-03-31 01:53:13 +0000

    Apply PHP-CS-Fixer changes

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace dutchie027\Test\EasyMySQLBackup;
 
-use dutchie027\EasyMySQLBackup\Log;
 use PHPUnit\Framework\TestCase;
 
 final class LogTest extends TestCase
--- tests/S3Test.php
+++ tests/S3Test.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace dutchie027\Test\EasyMySQLBackup;
 
-use dutchie027\EasyMySQLBackup\S3;
 use PHPUnit\Framework\TestCase;
 
 final class S3Test extends TestCase
