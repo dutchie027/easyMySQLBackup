803ecbcc3d913e53bf91c151581f51bb19d3f945
Justin Rodino <justin@227volts.com>
2022-04-03 21:42:22 -0400

    change to include new vars for tests

--- tests/BackupTest.php
+++ tests/BackupTest.php
@@ -14,9 +14,26 @@ final class BackupTest extends TestCase
      */
     private $backup;
 
+    /**
+     * @var string
+     */
+    private $tmp_ini;
+
     protected function setUp(): void
     {
-        $this->backup = new Backup();
+        $this->tmp_ini = tempnam(sys_get_temp_dir(), 'phpunit') ?: sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'php-unit';
+        $handle = fopen($this->tmp_ini, 'w');
+
+        if ($handle) {
+            fwrite($handle, '[s3]' . PHP_EOL);
+            fwrite($handle, "S3_REGION = 'us-east-3'" . PHP_EOL . PHP_EOL);
+            fwrite($handle, '[database]' . PHP_EOL);
+            fwrite($handle, "DB_USER = 'root'" . PHP_EOL);
+            fwrite($handle, "DB_PASSWORD = 'root'" . PHP_EOL);
+            fclose($handle);
+        }
+
+        $this->backup = new Backup($this->tmp_ini);
     }
 
     public function testCreateBackup(): void
--- tests/RestoreTest.php
+++ tests/RestoreTest.php
@@ -14,16 +14,33 @@ final class RestoreTest extends TestCase
      */
     private $backup;
 
+    /**
+     * @var string
+     */
+    private $tmp_ini;
+
     protected function setUp(): void
     {
-        $this->backup = new Backup();
+        $this->tmp_ini = tempnam(sys_get_temp_dir(), 'phpunit') ?: sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'php-unit';
+        $handle = fopen($this->tmp_ini, 'w');
+
+        if ($handle) {
+            fwrite($handle, '[s3]' . PHP_EOL);
+            fwrite($handle, "S3_REGION = 'us-east-3'" . PHP_EOL . PHP_EOL);
+            fwrite($handle, '[database]' . PHP_EOL);
+            fwrite($handle, "DB_USER = 'root'" . PHP_EOL);
+            fwrite($handle, "DB_PASSWORD = 'root'" . PHP_EOL);
+            fclose($handle);
+        }
+
+        $this->backup = new Backup($this->tmp_ini);
     }
 
     public function testRestore(): void
     {
         $buf = $this->backup->createLocalBackup('phpunit');
-        $this->backup->restore()->restoreLocalBackup($buf, 'phprest', 1);
-        $cmd = "mysql -u root -e 'show databases'";
+        $this->backup->restore()->restoreLocalBackup($buf, 'phprest', true);
+        $cmd = "mysql -uroot -proot -e 'show databases'";
         exec($cmd, $output, $code);
         self::assertContains('phprest', $output);
     }
